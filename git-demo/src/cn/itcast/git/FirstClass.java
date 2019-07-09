bafa37ef8b51649d2b3c093080754150d3d5d74b
feixiangzhang <3502223206@qq.com>
2019-07-09 19:27:28 +0800

    本地工程添加到git

---
+++ git-demo/src/cn/itcast/git/FirstClass.java
@@ -0,0 +1,14 @@
+package cn.itcast.git;
+
+public class FirstClass {
+
+	public static void main(String[] args) {
+
+		System.out.println("hello");
+		System.out.println("hello");
+		System.out.println("hello");
+		System.out.println("hello");
+		System.out.println("hello");
+		System.out.println("hello");
+	}
+}
